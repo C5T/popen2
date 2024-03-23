50d020ba47e13ce832b724e09c59407e066d0bb4
Dima Korolev <dmitry.korolev+github-2022@gmail.com>
2024-03-23 14:37:54 +0100

    Code v01

---
+++ popen2.h
@@ -0,0 +1,179 @@
+#pragma once
+
+#include <chrono>
+#include <iostream>
+
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/eventfd.h>
+#include <poll.h>
+
+#include <string>
+#include <thread>
+#include <functional>
+
+#include "bricks/strings/group_by_lines.h"
+
+template <typename F>
+static void MutableCStyleVectorStringsArg(const std::vector<std::string>& in, F&& f) {
+  std::vector<std::vector<char>> mutable_in;
+  mutable_in.resize(in.size());
+  for (size_t i = 0u; i < in.size(); ++i) {
+    mutable_in[i].assign(in[i].c_str(), in[i].c_str() + in[i].length() + 1u);
+  }
+  std::vector<char*> out;
+  for (auto& e : mutable_in) {
+    out.push_back(&e[0]);
+  }
+  out.push_back(nullptr);
+  f(&out[0]);
+}
+
+inline void pipe_and_keep_trying(int r[2]) {
+#if 0
+  int exponential_delay_ms = 5;
+  for (int i = 0; i < 20; ++i) {
+    int const e = pipe(r);
+    if (!e) {
+      return;
+    } if (errno == 23 || errno == 24) {
+      // "Too many open files", sigh, worth it to try again.
+      std::this_thread::sleep_for(std::chrono::milliseconds(exponential_delay_ms));
+      exponential_delay_ms += exponential_delay_ms/2;
+    } else {
+      break;
+    }
+  }
+  std::cerr << "FATAL: " << __LINE__ << std::endl;
+  ::abort();
+#else
+  if (pipe(r)) {
+    std::cerr << "FATAL: " << __LINE__ << std::endl;
+    ::abort();
+  }
+#endif
+}
+
+inline int popen2(std::vector<std::string> const& cmdline_,
+    std::function<void(const std::string&)> cb_line,
+    std::function<void(std::function<void(std::string const&)>, std::function<void()>)> cb_code,
+    std::vector<std::string> const& env_ = {}) {
+  pid_t pid;
+  int pipe_stdin[2];
+  int pipe_stdout[2];
+
+  pipe_and_keep_trying(pipe_stdin);
+  pipe_and_keep_trying(pipe_stdout);
+
+  int const efd = eventfd(0, 0);
+  if (efd < 0) {
+    std::cerr << "FATAL: " << __LINE__ << std::endl;
+    ::abort();
+  }
+
+  pid = fork();
+  if (pid < 0) {
+    std::cerr << "FATAL: " << __LINE__ << std::endl;
+    ::abort();
+  }
+
+  if (pid == 0) {
+    // Child.
+    ::close(pipe_stdin[1]);
+    dup2(pipe_stdin[0], 0);
+    ::close(pipe_stdout[0]);
+    dup2(pipe_stdout[1], 1);
+
+    if (env_.empty()) {
+      MutableCStyleVectorStringsArg(cmdline_, [&](char* const argv[]) {
+        int r = execvp(cmdline_[0].c_str(), argv);
+        std::cerr << "FATAL: " << __LINE__ << " R=" << r << ", errno=" << errno << std::endl;
+        perror("execvp");
+      });
+    } else {
+      MutableCStyleVectorStringsArg(cmdline_, [&](char* const argv[]) {
+        MutableCStyleVectorStringsArg(env_, [&](char* const envp[]) {
+          int r = execvpe(cmdline_[0].c_str(), argv, envp);
+          std::cerr << "FATAL: " << __LINE__ << " R=" << r << ", errno=" << errno << std::endl;
+          perror("execvpe");
+        });
+      });
+    }
+  }
+
+  ::close(pipe_stdin[0]);
+  ::close(pipe_stdout[1]);
+
+  std::thread thread_user_code(
+      [](std::function<void(std::function<void(std::string const&)>, std::function<void()>)> cb_code, int write_fd, int pid) {
+        cb_code(
+            [write_fd](std::string const& s) {
+              ssize_t const n = write(write_fd, s.c_str(), s.length());
+              if (n < 0 || static_cast<size_t>(n) != s.length()) {
+                return false;
+              } else {
+                return true;
+              }
+            },
+            [pid]() { kill(pid, SIGKILL); });
+      },
+      std::move(cb_code),
+      pipe_stdin[1],
+      pid);
+
+  std::thread thread_reader(
+      [](
+      std::function<void(const std::string&)> cb_line,
+      int read_fd, int efd
+      ) {
+        struct pollfd fds[2];
+        fds[0].fd = read_fd;
+        fds[0].events = POLLIN;
+        fds[1].fd = efd;
+        fds[1].events = POLLIN;
+
+        char buf[1000];
+
+        auto grouper = current::strings::CreateStatefulGroupByLines(std::move(cb_line));
+
+        while (true) {
+          ::poll(fds, 2, -1);
+          if (fds[0].revents & POLLIN) {
+            ssize_t const n = read(read_fd, buf, sizeof(buf) - 1);
+            if (n < 0) {
+              // NOTE(dkorolev): This may or may not be a major issue.
+              // std::cerr << __LINE__ << std::endl;
+              break;
+            }
+            buf[n] = '\0';
+            grouper.Feed(buf);
+          } else if (fds[1].revents & POLLIN) {
+            // NOTE(dkorolev): Termination signaled.
+            // std::cerr << __LINE__ << std::endl;
+            break;
+          }
+        }
+      },
+      std::move(cb_line),
+      pipe_stdout[0],
+      efd);
+
+  ::waitpid(pid, NULL, 0);
+
+  uint64_t u = 1;
+  if (write(efd, &u, sizeof(uint64_t)) != sizeof(uint64_t)) {
+    std::cerr << "FATAL: " << __LINE__ << std::endl;
+    ::abort();
+  }
+
+  thread_user_code.join();
+  thread_reader.join();
+
+  ::close(efd);
+
+  ::close(pipe_stdin[1]);
+  ::close(pipe_stdout[0]);
+
+  return 0;
+}
---
+++ popen2_test.cc
@@ -0,0 +1,122 @@
+#include <vector>
+#include <string>
+
+#include "popen2.h"
+
+#include "bricks/sync/waitable_atomic.h"
+
+#include "3rdparty/gtest/gtest-main-with-dflags.h"  // IWYU pragma: keep
+
+TEST(Popen2Test, Smoke) {
+  std::string result;
+  popen2(
+      {"/usr/bin/bash", "-c", "echo PASS"},
+      [&result](std::string const& line) { result = line; },
+      [](std::function<void(std::string const&)> write, std::function<void()> kill) {
+        static_cast<void>(write);
+        static_cast<void>(kill);
+      });
+  EXPECT_EQ(result, "PASS");
+}
+
+TEST(Popen2Test, SmokeWithDelay) {
+  std::string result;
+  popen2(
+      {"/usr/bin/bash", "-c", "sleep 0.01; echo PASS2"},
+      [&result](std::string const& line) { result = line; },
+      [](std::function<void(std::string const&)> write, std::function<void()> kill) {
+        static_cast<void>(write);
+        static_cast<void>(kill);
+      });
+  EXPECT_EQ(result, "PASS2");
+}
+
+TEST(Popen2Test, SmokeWithDelayInParentheses) {
+  std::string result;
+  popen2(
+      {"/usr/bin/bash", "-c", "(sleep 0.01; echo PASS3)"},
+      [&result](std::string const& line) { result = line; },
+      [](std::function<void(std::string const&)> write, std::function<void()> kill) {
+        static_cast<void>(write);
+        static_cast<void>(kill);
+      });
+  EXPECT_EQ(result, "PASS3");
+}
+
+// TODO(dkorolev): This test does not need the `WaitableAtomic`. A different one does.
+TEST(Popen2Test, ThreePrints) {
+  std::string result;
+  current::WaitableAtomic<int> c(0);
+  popen2(
+      {"/usr/bin/bash", "-c", "echo ONE; sleep 0.01; echo TWO; sleep 0.01; echo THREE"},
+      [&result, &c](std::string const& line) {
+        result += line + ' ';
+        ++*c.MutableScopedAccessor();
+      },
+      [](std::function<void(std::string const&)> write, std::function<void()> kill) {
+        static_cast<void>(write);
+        static_cast<void>(kill);
+      });
+  c.Wait([](int x) { return x == 3; });
+  ASSERT_EQ(result, "ONE TWO THREE ");
+}
+
+TEST(Popen2Test, KillsSuccessfully) {
+  bool nope = false;
+  popen2(
+      {"/usr/bin/bash", "-c", "sleep 10; echo NOPE"},
+      [&nope](std::string const&) { nope = true; },
+      [](std::function<void(std::string const&)>, std::function<void()> kill) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(5));
+        kill();
+      });
+  ASSERT_TRUE(!nope);
+}
+
+TEST(Popen2Test, ReadsStdin) {
+  std::string c;
+  popen2(
+      {"/usr/bin/bash", "-c", "read A; read B; echo $((A+B))"},
+      [&c](std::string const& line) { c = line; },
+      [](std::function<void(std::string const&)> write, std::function<void()>) { write("1\n2\n"); });
+  ASSERT_EQ(c, "3");
+}
+
+TEST(Popen2Test, ReadsStdinForever) {
+  std::string result = "result:";
+  popen2(
+      {"/usr/bin/bash", "-c", "while true; do read A; read B; C=$((A+B)); if [ $C == '0' ]; then exit; fi; echo $C; done"},
+      [&result](std::string const& line) { result += ' ' + line; },
+      [](std::function<void(std::string const&)> write, std::function<void()>) { write("1\n2\n3\n4\n0\n0\n"); });
+  ASSERT_EQ(result, "result: 3 7");
+}
+
+TEST(Popen2Test, MultipleOutputLines) {
+  std::string result = "result:";
+  popen2(
+      {"/usr/bin/bash", "-c", "seq 10"},
+      [&result](std::string const& line) { result += ' ' + line; },
+      [](std::function<void(std::string const&)> write, std::function<void()>) { write("1\n2\n"); });
+  ASSERT_EQ(result, "result: 1 2 3 4 5 6 7 8 9 10");
+}
+
+TEST(Popen2Test, MultipleOutputLinesWithMath) {
+  std::string result = "result:";
+  popen2(
+      {"/usr/bin/bash", "-c", "for i in $(seq 3 7) ; do echo $((i * i)) ; done"},
+      [&result](std::string const& line) { result += ' ' + line; },
+      [](std::function<void(std::string const&)> write, std::function<void()>) { write("1\n2\n"); });
+  ASSERT_EQ(result, "result: 9 16 25 36 49");
+}
+
+// TODO(dkorolev): This test does not need the `WaitableAtomic`.
+TEST(Popen2Test, Env) {
+  std::string result;
+  current::WaitableAtomic<std::string> wa;
+  popen2(
+      {"/usr/bin/bash", "-c", "echo $FOO"},
+      [&result](std::string const& s) { result = s; },
+      [](std::function<void(std::string const&)>, std::function<void()>) {},
+      {"FOO=bar"});
+  ASSERT_TRUE(result == "bar");
+}
